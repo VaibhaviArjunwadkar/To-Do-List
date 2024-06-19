3520d1017362e97625d43010c70d4fa54f3c9744
Vaibhavi Arjunwadkar <31095655+VaibhaviArjunwadkar@users.noreply.github.com>
2024-06-19 13:16:57 -0500

    updated mongoDB connection

--- server.js
+++ server.js
@@ -5,22 +5,20 @@ const schema = require('./schema/schema');
 
 const app = express();
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/todolist', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
 
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
-});
+//connection to mongodb database code 
+var db = 'mongodb://127.0.0.1:27017/todolist';
+mongoose.connect(db).then(()=>console.log('connected to MongoDB')).catch((err)=>console.log('error'));
+
 
 // Use GraphQL middleware
-app.use('/graphql', graphqlHTTP({
+app.use('/', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
+
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
 });
+
